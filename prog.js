73ea9b1c295c884445c2a34afe0c92363f77808d
mouheb864 <80130808+mouheb864@users.noreply.github.com>
2021-05-09 15:46:51 +0200

    Add files via upload

---
+++ prog.js
@@ -0,0 +1,16 @@
+function myFunction() {
+var str="Découvrir notre école";
+ var result = str.link("w.html");
+  document.getElementById("demo").innerHTML = result
+}
+function myFunction1() {
+  var str="Inscrire votre enfant";
+var result = str.link("hy.html");
+ document.getElementById("demo1").innerHTML = result
+
+}
+function myFunction2() {
+  var str="Vivez l'expérience";
+   var result = str.link("robotique.html");
+    document.getElementById("demo2").innerHTML = result
+  }
